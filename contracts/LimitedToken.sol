f0005f8b21e9d23f5b3951e84b0f8239b502dfc7
Soula <helloworldwithsoulas@gmail.com>
2021-09-08 13:56:23 +0900

    feat: test for protected/private method

--- contracts/LimitedToken.sol
+++ contracts/LimitedToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./AccessControl.sol";
 
 contract LimitedToken is ERC20, AccessControl {
-  bool public isLimited = true;
+  bool public isLimited = false;
 
   constructor(string memory name_, string memory symbol_)
     ERC20(name_, symbol_)
--- test/LimitedToken.js
+++ test/LimitedToken.js
@@ -1,4 +1,4 @@
-const LimitedToken = artifacts.require("LimitedToken");
+const LimitedToken = artifacts.require("LimitedTokenForTest");
 const { expect } = require("chai");
 const truffleAssert = require("truffle-assertions");
 
@@ -11,7 +11,25 @@ contract("LimitedToken", (accounts) => {
     instance = await LimitedToken.new(tokenName, tokenSymbol);
   });
 
-  describe("Unlock method", async () => {
+  describe("Lock method", () => {
+    it("should be executed by contract owner", async () => {
+      await truffleAssert.passes(instance.BypassedLock.sendTransaction());
+    });
+
+    it("should not be executed by non-owner users", async () => {
+      await truffleAssert.reverts(
+        instance.BypassedLock.sendTransaction({ from: accounts[1] })
+      );
+    });
+
+    it("should set the limitation status true", async () => {
+      expect(await instance.isLimited.call()).to.eq(false);
+      await instance.BypassedLock.sendTransaction();
+      expect(await instance.isLimited.call()).to.eq(true);
+    });
+  });
+
+  describe("Unlock method", () => {
     it("should be executed by contract owner", async () => {
       await truffleAssert.passes(instance.Unlock.sendTransaction());
     });
@@ -23,6 +41,7 @@ contract("LimitedToken", (accounts) => {
     });
 
     it("should set the limitation status false", async () => {
+      await instance.BypassedLock.sendTransaction();
       expect(await instance.isLimited.call()).to.eq(true);
       await instance.Unlock.sendTransaction();
       expect(await instance.isLimited.call()).to.eq(false);

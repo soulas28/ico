96f3af5b3e7535664eaea7e2e39d9a4fc5311a93
Soula <helloworldwithsoulas@gmail.com>
2021-09-28 15:58:55 +0900

    chore: inject bundled js file into html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './web-interface/src/App.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'output', 'web-interface'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(
+        __dirname,
+        'web-interface',
+        'assets',
+        'index.html'
+      ),
+    }),
+  ],
+  target: 'web',
 }

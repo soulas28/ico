ecc89a1955cd040f70ef8321751a0b7e38611af4
Soula <helloworldwithsoulas@gmail.com>
2021-09-28 17:27:03 +0900

    chore: dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,15 @@
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
+const path = require('path')
 
 const config = merge(baseConfig, {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    compress: true,
+    liveReload: true,
+    port: 8080,
+  },
 })
 
 module.exports = config

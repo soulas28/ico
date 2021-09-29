13d49d60a38b1b2e6393de3a3f76b3073079909c
Soula <helloworldwithsoulas@gmail.com>
2021-09-29 12:26:14 +0900

    chore: trigger eslint inside of webpack

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 const config = {
   mode: 'production',
@@ -37,6 +38,7 @@ const config = {
     path: path.resolve(__dirname, 'output', 'web-interface'),
   },
   plugins: [
+    new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(

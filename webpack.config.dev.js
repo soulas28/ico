d66e166823d34d79a1f00afaf511b437988845ae
Soula <helloworldwithsoulas@gmail.com>
2021-09-29 12:29:40 +0900

    chore: webpack speed measuring

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,13 +2,19 @@
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
-const config = merge(baseConfig, {
-  mode: 'development',
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    compress: true,
-    liveReload: true,
-    port: 8080,
-  },
-})
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+
+const smp = new SpeedMeasurePlugin()
+
+const config = smp.wrap(
+  merge(baseConfig, {
+    mode: 'development',
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+      compress: true,
+      liveReload: true,
+      port: 8080,
+    },
+  })
+)
 module.exports = config
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
-const config = merge(baseConfig, {
-  mode: 'production',
-})
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+
+const smp = new SpeedMeasurePlugin()
+
+const config = smp.wrap(
+  merge(baseConfig, {
+    mode: 'production',
+  })
+)
 
 module.exports = config

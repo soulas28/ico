aa553135bea027990e97a4e1b8bafc4e1dbe9e1b
Soula <helloworldwithsoulas@gmail.com>
2021-09-30 17:08:38 +0900

    chore: css loader

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -32,6 +32,11 @@ const config = {
           },
         },
       },
+      {
+        test: /.*\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   output: {

fb9aa31999b6e3c878210c66bca92df4a5c03493
SoulaS <helloworldwithsoulas@gmail.com>
2022-01-01 16:49:56 +0900

    feat: migrations func of ICO main contract

---
+++ migrations/2_ICO_migration.js
@@ -0,0 +1,24 @@
+const ICO = artifacts.require("ICO");
+
+const name = process.env.NAME || "";
+const symbol = process.env.SYMBOL || "";
+const periodBlock = process.env.PERIOD_BLOCK || "";
+const numPeriods = process.env.NUM_PERIODS || "";
+const unitPeriodBalance = process.env.UNIT_PERIOD_BALANCE || "";
+const rate = process.env.RATE || "";
+const tokensForOwner = process.env.TOKENS_FOR_OWNER || "";
+const withdrawLimit = process.env.WITHDRAW_LIMIT || "";
+
+module.exports = function (deployer) {
+  deployer.deploy(
+    ICO,
+    name,
+    symbol,
+    periodBlock,
+    numPeriods,
+    unitPeriodBalance,
+    rate,
+    tokensForOwner,
+    withdrawLimit
+  );
+};
